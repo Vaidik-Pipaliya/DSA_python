fcaaf984f05a997bbf1b701c7b6e59fd964c875d
VD <vaidikpipaliya@gmail.com>
2026-06-26 11:21:35 +0530

    Time: 27 ms (7.63%), Space: 12.4 MB (17.46%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,12 +1,19 @@
 class Solution(object):
-    def isPalindrome(self, x):
-        if x< 0:
-            return False
-        num =x
+    def isPalindrome(self , x):
+        num = x
         result = 0
+        if num < 0:
+            return False
         while num > 0:
             last_digit = num % 10
             result = (result * 10) + last_digit
             num = num // 10
-        return x == result
+        return result == x
+
+
+
+"""class Solution(object):
+    def isPalindrome(self, x):
+        a = str(x)
+        return a == a[::-1]"""
         

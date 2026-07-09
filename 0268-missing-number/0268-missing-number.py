d4f9b9858ba00bbbb355cf317f18a5fca985547a
VD <vaidikpipaliya@gmail.com>
2026-07-09 17:17:01 +0530

    Time: 0 ms (100%), Space: 13.2 MB (67.19%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,13 +1,6 @@
 class Solution(object):
     def missingNumber(self, nums):
         n = len(nums)
-        dict = {}
-        for i in range (0,n+1):
-            dict[i] = 0
-        for num in nums:
-            dict[num]=1
-        for k,v in dict.items():
-            if v == 0:
-                return k
+        return (n*(n+1))//2 - sum(nums)
 
         
